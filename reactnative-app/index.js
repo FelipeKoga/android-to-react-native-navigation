ae4bd4cae511b463af85c449e682fb15765b3aee
koga <kosloski.fkoga@gmail.com>
2023-01-24 19:52:22 -0300

    setup react native project

---
+++ reactnative-app/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from "react-native";
+
+const HelloWorld = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.hello}>Hello World from React Native</Text>
+        </View>
+    )
+}
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    hello: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10
+    }
+});
+
+AppRegistry.registerComponent(
+    "pudim_react_native",
+    () => HelloWorld
+)
