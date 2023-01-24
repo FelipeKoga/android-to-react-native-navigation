9420ac69457d542c0bfbee85c98d5e6f48891877
koga <kosloski.fkoga@gmail.com>
2023-01-24 19:49:10 -0300

    fix: change main activity path

--- android/app/src/main/java/com/koga/poc_native_app_integrations/ui/MainActivity.kt
+++ android/app/src/main/java/com/koga/poc_native_app_integrations/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.koga.poc_native_app_integrations
+package com.koga.poc_native_app_integrations.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.koga.poc_native_app_integrations.ui.theme.PocnativeappintegrationsTheme
 
 class MainActivity : ComponentActivity() {
